05bc198275913c066e65032cda8ff8be4ddcc6f8
Zeeshan Saleem <m.zeeshan.saleem97@gmail.com>
2026-04-18 12:50:43 +0200

    create image folder and create data.ts and product.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { productSampleData } from './data'
 
 function App() {
 
@@ -9,7 +10,19 @@ function App() {
         <h1>Header</h1>
       </header>
       <main>
-        <h1>Main Content</h1>
+        <ul>
+
+        {productSampleData.map((product)=>(
+          <li key={product.slug}>
+            <h2>{product.name}</h2>
+            <p>${product.price.toFixed(2)}</p>
+            <a href="/">
+              <img src={product.image} alt={product.name} width={200} />
+            </a>
+            <p>{product.description}</p>
+          </li>
+        ))}
+        </ul>
       </main>
       <footer>
         <h1>Footer</h1>
