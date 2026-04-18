44c0b4d7b93a6fd958185d5372051a12fcd303ac
Zeeshan Saleem <m.zeeshan.saleem97@gmail.com>
2026-04-18 16:04:39 +0200

    product card complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,62 @@
-
-import './App.css'
-import { productSampleData } from './data'
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Nav,
+  Navbar,
+  Row,
+} from "react-bootstrap";
+import { productSampleData } from "./data";
 
 function App() {
-
   return (
     <>
-      <header>
-        <h1>Header</h1>
-      </header>
-      <main>
-        <ul>
-
-        {productSampleData.map((product)=>(
-          <li key={product.slug}>
-            <h2>{product.name}</h2>
-            <p>${product.price.toFixed(2)}</p>
-            <a href="/">
-              <img src={product.image} alt={product.name} width={200} />
-            </a>
-            <p>{product.description}</p>
-          </li>
-        ))}
-        </ul>
-      </main>
-      <footer>
-        <h1>Footer</h1>
-      </footer>
+      <div className="d-flex flex-column vh-100">
+        <header>
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+              <Navbar.Brand href="#home">Amazee</Navbar.Brand>
+            </Container>
+            <Nav className="me-auto">
+              <Nav.Link href="/home">Home</Nav.Link>
+              <Nav.Link href="/products">Products</Nav.Link>
+              <Nav.Link href="/contact">Cart</Nav.Link>
+            </Nav>
+          </Navbar>
+        </header>
+        <main className="main">
+          <Container fluid className="mt-3">
+            <Row>
+              {productSampleData.map((product) => (
+                <Col key={product.slug} sm={6} md={4} lg={3} className="mb-4">
+                  <Card style={{ width: "100%" }}>
+                    <div className="image_container">
+                      <Card.Img
+                        className="card-img-top"
+                        variant="top"
+                        src={product.image}
+                      />
+                    </div>
+                    <Card.Body>
+                      <Card.Title>{product.name}</Card.Title>
+                      <Card.Text>{product.description}</Card.Text>
+                      <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        </main>
+        <footer>
+          <div>
+            <p className="text-center">© 2024 Amazee. All rights reserved.</p>
+          </div>
+        </footer>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
